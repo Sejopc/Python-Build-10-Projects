6ab78a8a18c632c77c1f48c965ca0e7eb93b31f6
Jose Peralta Coronado <jperaltacoronado@netsuite.com>
2021-07-16 22:50:07 -0600

    Finished Making an Android APK File from the Kivy App Section

--- Object-Oriented Programming (OOP)/Exercise-Rewriting_the_app_using_Classes_and_OOP.py
+++ Object-Oriented Programming (OOP)/Exercise-Rewriting_the_app_using_Classes_and_OOP.py
@@ -1,2 +1,3 @@
 # Alter the frontend.py script containing the GUI code by changing its functional-oriented design into an OOP design. 
 
+# I placed these files in a subfolder named "Exercise-Rewriting-the-App-Using-Classes-and-OOP"
